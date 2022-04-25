d522a4546a1af460ac22bace17538e015a049656
wogns1026 <sksmsljh1026@gmail.com>
2022-04-25 21:39:37 +0900

    n_ru project setting

---
+++ src/main/java/com/example/n_ru/NRuApplication.java
@@ -0,0 +1,13 @@
+package com.example.n_ru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NRuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NRuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/n_ru/NRuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.n_ru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NRuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
